257106464ab65d00b9db134eb07a5e659b507a6a
Ilya <iliayar3@gmail.com>
2021-04-14 22:00:57 +0300

    Gradient descent plotting fine

--- scripts/lab2/main.py
+++ scripts/lab2/main.py
@@ -8,34 +8,55 @@ def get_func(A, b, c):
     A = np.array(A)
     b = np.array(b)
     def f(x):
-        return 1/2 * (A * x).dot(x) + b.dot(x) + c
+        return 1/2 * A.dot(x).dot(x) + b.dot(x) + c
     return f
 
+METHODS = [
+    Method.STEEPEST_DESCENT,
+    Method.GRADIENT_DESCENT,
+    Method.CONJUGATE_GRADIENT
+]
+
+
 if __name__ == '__main__':
-    A = [[2, -1, 2],
-         [-1, 1, -3],
-         [2, -3, 11]]
-    b = [-100, 1, -30]
-    c =10
-    x, f, data = read_data(A, b, c, Method.GRADIENT_DESCENT)
+    # A = [[2, -1, 2],
+    #      [-1, 1, -3],
+    #      [2, -3, 11]]
+    # b = [-100, 1, -30]
+    # c = 10
+    A = [[2, -1],
+         [-1, 1]]
+    b = [-100, 1]
+    c = 10
+    x, f, data = read_data(A, b, c, METHODS[2])
     func = get_func(A, b, c)
     # print(x, f, data)
+    
     print(len(data))
-    exit(0)
-    STEP = 200
+    STEP = 1
     i = STEP - 1
-    for d in data:
+    # last = 100
+    # skip = len(data) - last
+    levels = []
+    for (p, grad) in data:
+        plt.arrow(p[0], p[1], -grad[0], -grad[1], head_width=0.4, width=0.1, color='red')
         i = (i + 1) % STEP
+        # skip -= 1
+        # if skip > 0:
+        #     continue
         if i != 0:
             continue
-        p, grad = d
-        Z = np.linspace(p[2] - 100, p[2] + 100, 100)
-        F = [func((p[0], p[1], z)) for z in Z]
-        plt.plot(Z, F, color='blue')
-        # f = func(p)
-        # plt.plot(p, [f, f, f], marker='.')
-        # plt.scatter([x[0]], [x[1]])
-        plt.arrow(p[0], p[1], grad[0], grad[1], head_width=0.2, width=0.05)
+        levels.append(func(p))
+    levels.sort()
+    X = np.linspace(0, 200, 100)
+    Y = np.linspace(0, 200, 100)
+    Z = []
+    for x in X:
+        z = []
+        for y in Y:
+            z.append(func((x, y)))
+        Z.append(z)
+    plt.contour(X, Y, Z, levels = levels)
     plt.show()
     
     
