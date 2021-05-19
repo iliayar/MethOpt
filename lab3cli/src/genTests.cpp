7a79214493ebc1761574db12bfe420c497808f00
Ilya <iliayar3@gmail.com>
2021-05-20 01:01:25 +0300

    Fix linking

--- lab3cli/src/genTests.cpp
+++ lab3cli/src/genTests.cpp
@@ -8,7 +8,7 @@ int tests_main(std::string file = fire::arg({"-f", "--file", "file prefix"})
                , int kmax = fire::arg({"-c", "--count", "Tests count"})
                , int n = fire::arg({"-n", "dimension"})
                , int dist = fire::arg({"-d", "--dist", "The distance from main diag where elements are not zero"})
-               , bool hilberts = fire::arg({"-h", "--hilbert", "Generate Hilbert matrice"})) {
+               , bool hilberts = fire::arg({"--hilbert", "Generate Hilbert matrice"})) {
     generate_tests(file, kmax, n, dist);
     return 0;
 }
