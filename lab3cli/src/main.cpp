f54df377af7482b5b7978948fbfafc1faf8c4ca4
kruxx <0088664@mail.ru>
2021-05-19 23:16:03 +0300

    Gauss method's modification

--- lab3cli/src/main.cpp
+++ lab3cli/src/main.cpp
@@ -7,16 +7,25 @@
 int fired_main(std::string file = fire::arg({"-f", "--file", "The input file"})) {
     std::ifstream input(file);
     ProfileMatrix<double> matrix(input);
+    PrimitiveMatrix<double> primitive_matrix = PrimitiveMatrix<double>(matrix);
     std::vector<double> input_vector(matrix.size());
     for (double &x : input_vector) {
         double t;
         input >> t;
         x = t;
     }
+    std::vector<double> input_vector_copy = input_vector;
     LUDecomposition<double> lu(std::move(matrix));
-    std::vector<double> t = gauss_bottom_triangle(lu, input_vector);
-    std::vector<double> result = gauss_upper_triangle(lu, t);
-    for (double &x : result) {
+    auto t = gauss_bottom_triangle(lu, input_vector);
+    std::cout << "System of linear equations solution with LU decomposition method" << std::endl;
+    for (double &x : gauss_upper_triangle(lu, t)) {
+        std::cout << x << ' ';
+    }
+    std::cout << std::endl;
+
+    std::cout << "System of linear equations solution with Gauss method's modification (main element selection)"
+              << std::endl;
+    for (double &x : gauss_main_element(primitive_matrix, input_vector_copy)) {
         std::cout << x << ' ';
     }
     return 0;
--- liblab3/include/gauss.hpp
+++ liblab3/include/gauss.hpp
@@ -3,15 +3,21 @@
 // destroys input vector
 template<typename T>
 std::vector<T> gauss_bottom_triangle(LUDecomposition<T> &lu, std::vector<T> &vec) {
+    auto temp = L_matrix_proxy<T>(lu);
+    return gauss_bottom_triangle(temp, vec);
+}
+
+template<typename T>
+std::vector<T> gauss_bottom_triangle(Matrix<T> &lu, std::vector<T> &vec) {
     int size = vec.size();
     std::vector<T> result(size);
     for (int i = 0; i < size; ++i) {
         for (int j = i + 1; j < size; ++j) {
-            T c = lu.getInL(j, i) / lu.getInL(i, i);
+            T c = lu.get(j, i) / lu.get(i, i);
             vec[j] = vec[j] - c * vec[i];
-//            lu.getInL(j, i) = 0;
+//            lu.get(j, i) = 0;
         }
-        result[i] = vec[i] / lu.getInL(i, i);
+        result[i] = vec[i] / lu.get(i, i);
     }
     return result;
 }
@@ -19,16 +25,50 @@ std::vector<T> gauss_bottom_triangle(LUDecomposition<T> &lu, std::vector<T> &vec
 // destroys input vector
 template<typename T>
 std::vector<T> gauss_upper_triangle(LUDecomposition<T> &lu, std::vector<T> &vec) {
+    auto temp = U_matrix_proxy<T>(lu);
+    return gauss_upper_triangle(temp, vec);
+}
+
+// destroys input vector
+template<typename T>
+std::vector<T> gauss_upper_triangle(Matrix<T> &lu, std::vector<T> &vec) {
     int size = vec.size();
     std::vector<T> result(size);
     for (int i = size - 1; i >= 0; --i) {
         for (int j = i - 1; j >= 0; --j) {
-//            in lu always 1
-            T c = lu.getInU(j, i) / lu.getInU(i, i);
+//            in lu always 1 but here we working with an unspecified matrix
+            T c = lu.get(j, i) / lu.get(i, i);
             vec[j] = vec[j] - c * vec[i];
-//            lu.getInU(j, i) = 0;
+//            lu.get(j, i) = 0;
         }
-        result[i] = vec[i] / lu.getInU(i, i);
+        result[i] = vec[i] / lu.get(i, i);
     }
     return result;
+}
+
+template<typename T>
+std::vector<T> gauss_main_element(PrimitiveMatrix<T> &matrix, std::vector<T> &vec) {
+    int size = vec.size();
+    for (int i = 0; i < size; ++i) {
+        int max_el_row = i;
+        T max = matrix.get(i, i);
+        for (int j = i + 1; j < size; ++j) {
+            if (matrix.get(j, i) > max) {
+                max = matrix.get(j, i);
+                max_el_row = j;
+            }
+        }
+        matrix.swap_rows(i, max_el_row);
+        std::swap(vec[i], vec[max_el_row]);
+
+        for (int j = i + 1; j < size; ++j) {
+            T c = matrix.get(j, i) / matrix.get(i, i);
+            vec[j] = vec[j] - c * vec[i];
+//            matrix.get(j, i) = 0;
+            for (int k = i + 1; k < size; ++k) {
+                matrix.get(j, k) = matrix.get(j, k) - c * matrix.get(i, k);
+            }
+        }
+    }
+    return gauss_upper_triangle(matrix, vec);
 }
