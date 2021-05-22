f42b715717c79134a2e689597c900095087b264c
kruxx <0088664@mail.ru>
2021-05-23 00:25:35 +0300

    Some comments for gauss.hpp

--- liblab3/include/gauss.hpp
+++ liblab3/include/gauss.hpp
@@ -1,17 +1,25 @@
 #include "matrix.hpp"
 
 /**
- * Solve the equation y = Ux, i.e perform the reverse Gaussian. destroys input vector
+ * Solves the equation y = Ux, where U is a matrix in the upper triangular form,
+ * i.e performs the reverse Gaussian. Destroys input vector, does not change
+ * given matrix.
  * @param lu The upper triangle matrix in LU decomposition
  * @param vec The x vector in the equation
  */
-template<typename T>
-std::vector<T> gauss_upper_triangle(AbstractMatrix<T> &lu, std::vector<T> &vec) {
+template <typename T>
+std::vector<T> gauss_upper_triangle(AbstractMatrix<T> &lu,
+                                    std::vector<T> &vec) {
+    //    computing complexity (muls & divs): sum i = 1 to n (2i + 1) =
+    //    = n + 2 * (1 + n) * n / 2 =
+    //    = n ^ 2 + 2n;
+    //    [assuming lu specification] = (n ^ 2 + 3n) / 2
     int size = vec.size();
     std::vector<T> result(size);
     for (int i = size - 1; i >= 0; --i) {
         for (int j = i - 1; j >= 0; --j) {
-//            in lu always 1 but here we working with an unspecified matrix
+            //            optimization for lu method (can be performed by
+            //            the compiler): c = U[j][i];
             T c = lu.get(j, i) / lu.get(i, i);
             vec[j] = vec[j] - c * vec[i];
         }
@@ -21,14 +29,19 @@ std::vector<T> gauss_upper_triangle(AbstractMatrix<T> &lu, std::vector<T> &vec) 
 }
 
 /**
- * Solve the equation Ly = b, i.e perform direct Gaussian. destroys input vector
+ * Solves the equation Ly = b, where L is a matrix in the lower triangular
+ * form, i.e performs direct Gaussian. Destroys input vector, does not change
+ * given matrix.
  * @param lu The left triangle matrice in LU decomposition
  * @prama vec The b vector in equiation
  */
-template<typename T>
-std::vector<T> gauss_bottom_triangle(AbstractMatrix<T> &lu, std::vector<T> &vec) {
+template <typename T>
+std::vector<T> gauss_bottom_triangle(AbstractMatrix<T> &lu,
+                                     std::vector<T> &vec) {
+    //    computing complexity: n ^ 2 + 2n (calculations are the same as above)
     int size = vec.size();
     std::vector<T> result(size);
+    //    method is simplified
     for (int i = 0; i < size; ++i) {
         for (int j = i + 1; j < size; ++j) {
             T c = lu.get(j, i) / lu.get(i, i);
@@ -40,30 +53,40 @@ std::vector<T> gauss_bottom_triangle(AbstractMatrix<T> &lu, std::vector<T> &vec)
 }
 
 /**
- * Helper funtion for {@link #gauss_upper_triangle(AbstractMatrix<T>&, std::vector<T>&)}
+ * Helper function for {@link #gauss_upper_triangle(AbstractMatrix<T>&,
+ * std::vector<T>&)}
  */
-template<typename T>
-std::vector<T> gauss_upper_triangle(LUDecomposition<T> &lu, std::vector<T> &vec) {
+template <typename T>
+std::vector<T> gauss_upper_triangle(LUDecomposition<T> &lu,
+                                    std::vector<T> &vec) {
     auto temp = U_matrix_proxy<T>(lu);
     return gauss_upper_triangle(temp, vec);
 }
 
 /**
- * Helper funtion for {@link #gauss_bottom_triangle(AbstractMatrix<T>&, std::vector<T>&)}
+ * Helper function for {@link #gauss_bottom_triangle(AbstractMatrix<T>&,
+ * std::vector<T>&)}
  */
-template<typename T>
-std::vector<T> gauss_bottom_triangle(LUDecomposition<T> &lu, std::vector<T> &vec) {
+template <typename T>
+std::vector<T> gauss_bottom_triangle(LUDecomposition<T> &lu,
+                                     std::vector<T> &vec) {
     auto temp = L_matrix_proxy<T>(lu);
     return gauss_bottom_triangle(temp, vec);
 }
 
 /**
- * Solve the system using Gausse method with choice of main element.
+ * Solve the system using Gauss method with choice of main element.
  * @param matrix The matrix in natural form
- * @param vec The b vector in equition Ax = b
+ * @param vec The b vector in the equation Ax = b
  */
-template<typename T>
-std::vector<T> gauss_main_element(PrimitiveMatrix<T> &matrix, std::vector<T> &vec) {
+template <typename T>
+std::vector<T> gauss_main_element(PrimitiveMatrix<T> &matrix,
+                                  std::vector<T> &vec) {
+    //    computing complexity:
+    //    sum i = 1 to n (sum j = i + 2 to n (2 + sum k = i + 2 to n (1))) =
+    //    sum i = 1 to n (sum j = i + 2 to n (2 + n - i - 2) =
+    //    sum i = 1 to n (sum j = i + 2 to n (n - i) =
+    //    n * (2 - 3 n + n ^ 2) / 3
     int size = vec.size();
     for (int i = 0; i < size; ++i) {
         int max_el_row = i;
