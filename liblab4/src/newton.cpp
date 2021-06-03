013bfc08a0a07eaf3261528061b753c6ac4ad90e
interestIngc <58723287+interestIngc@users.noreply.github.com>
2021-06-03 20:41:16 +0300

    fixed_newton

--- liblab4/src/newton.cpp
+++ liblab4/src/newton.cpp
@@ -1,15 +1,16 @@
 #include "newton.h"
 
 int main() {
-    newton_ordinary<double> method;
-    std::vector<Vector<double>> keks = {
+    newton_with_descent<double> method;
+    std::vector<Vector<double>> v = {
             Vector<double>({2,  -1, 2}),
             Vector<double>({-1, 1, -3}),
             Vector<double>({2,  -3, 11})
     };
-    Vector<Vector<double>> m = Vector<Vector<double>>(keks);
     Vector<double> vect = {-100, 1, -30};
-    Matrix<double> matrix = Matrix<double>(m);
+    Matrix<double> matrix = Matrix<double>(Vector<Vector<double>>(v));
     quad_multivariate_function<double> function(QuadFunction<double>(matrix, vect, 10));
-    std::cout << method.find(function, {1, 1, 1}, 1e-4).second;
+    auto res = method.find(function, {1, 1, 1}, 1e-4);
+    std::cout << res.second << "\n";
+    std::cout << res.first << "\n";
 }
