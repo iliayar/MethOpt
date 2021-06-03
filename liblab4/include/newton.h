bcf256bf6556fb285b33ed37a97dd6a00cd190f2
paskudnicc <0088664@mail.ru>
2021-06-03 14:28:47 +0300

    lab4 base
    yeah its lab2 with some tweaks

---
+++ liblab4/include/newton.h
@@ -0,0 +1,28 @@
+#include "../../lib/include/multi_methods.hpp"
+
+template <typename T>
+struct multivariate_function {
+
+//    copy - it should be changeable
+    Vector<T> get_grad();
+
+//    copy - it should be changeable
+    Matrix<T> get_hessian();
+};
+
+template <typename T, typename IterData>
+struct mf_optimize {
+    const int MAX_ITERATIONS = 1e+6;
+
+    virtual std::pair<Vector<T>, T> find(const multivariate_function<T>& func, const Vector<T>& init_point, const T eps) = 0;
+
+    bool iter(IterData i) {
+        m_data.push_back(std::move(i));
+        return m_data.size() <= MAX_ITERATIONS;
+    }
+
+    const std::vector<IterData>& get_data() { return m_data; }
+
+private:
+    std::vector<IterData> m_data;
+};
